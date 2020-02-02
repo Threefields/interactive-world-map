dfa5a44a869af2e7cd6acf87e2bea001f5c1f35f
Stefanos Trialonis <strialonis@gmail.com>
2020-02-02 17:27:24 +0100

    chore: minor adjustments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 import 'leaflet/dist/leaflet.css'
 
 import L from 'leaflet'
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map as LeafletMap, TileLayer } from 'react-leaflet'
 import Timeline from 'react-calendar-timeline'
 import 'react-calendar-timeline/lib/Timeline.css'
 
@@ -18,12 +18,20 @@ L.Icon.Default.mergeOptions({
 
 const styles = {
   mapContainer: {
-    height: 'calc(100% - 324px)',
-    width: '100%'
+    height: '100%',
+    width: '100%',
+    zIndex: '1',
+  },
+  timelineContainer: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
+    zIndex: '2',
+    background: '#fff',
   }
 }
 
-const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }]
+const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }, { id: 3, title: 'group 3' }, { id: 4, title: 'group 4' }]
 
 const items = [
   {
@@ -31,50 +39,60 @@ const items = [
     group: 1,
     title: 'item 1',
     start_time: moment(),
-    end_time: moment().add(1, 'hour')
+    end_time: moment().add(1, 'hour'),
+    canMove: false,
   },
   {
     id: 2,
     group: 2,
     title: 'item 2',
     start_time: moment().add(-0.5, 'hour'),
-    end_time: moment().add(0.5, 'hour')
+    end_time: moment().add(0.5, 'hour'),
+    canMove: false,
   },
   {
     id: 3,
     group: 1,
     title: 'item 3',
     start_time: moment().add(2, 'hour'),
-    end_time: moment().add(3, 'hour')
+    end_time: moment().add(3, 'hour'),
+    canMove: false,
+  },
+  {
+    id: 4,
+    group: 3,
+    title: 'item 4',
+    start_time: moment().add(2, 'hour'),
+    end_time: moment().add(3, 'hour'),
+    canMove: false,
+  },
+  {
+    id: 5,
+    group: 4,
+    title: 'item 5',
+    start_time: moment().add(2, 'hour'),
+    end_time: moment().add(3, 'hour'),
+    canMove: false,
   }
 ]
 
 function App() {
-  const location = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13,
-  }
-
   return (
     <>
-      <LeafletMap center={[location.lat, location.lng]} zoom={location.zoom} style={styles.mapContainer}>
+      <LeafletMap center={[51.505, -0.09]} style={styles.mapContainer} minZoom={4} maxZoom={6} zoom={4} zoomControl={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[location.lat, location.lng]}>
-          <Popup>
-            A pretty CSS3 popup. <br/> Easily customizable.
-          </Popup>
-        </Marker>
       </LeafletMap>
-      <Timeline
-        groups={groups}
-        items={items}
-        defaultTimeStart={moment().add(-12, 'hour')}
-        defaultTimeEnd={moment().add(12, 'hour')}
-      />
+      <div style={styles.timelineContainer}>
+        <Timeline
+          groups={groups}
+          items={items}
+          defaultTimeStart={moment().add(-12, 'hour')}
+          defaultTimeEnd={moment().add(12, 'hour')}
+        />
+      </div>
     </>
   )
 }
